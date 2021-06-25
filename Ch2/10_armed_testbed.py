476c3a3b3dfa05bc5d3098d4fccd9805cb32b11f
Aviral Gupta <aviralg1975@gmail.com>
2021-06-25 19:26:11 +0530

    Refactor

--- Ch2/10_armed_testbed.py
+++ Ch2/10_armed_testbed.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 import matplotlib as mpt
+from matplotlib import pyplot as plt
+from statistics import mean
+
 
 BANDIT_SIZE = 10
 TIME_STEPS  = 1000
@@ -10,65 +13,80 @@ class Bandit:
     def __init__(self, epsilon = 0):
         # @q      : Real reward value of actions
         # @epsilon: probability for exploration
-        # @qt     : Reward value of action till a particular step 
+        # @qt     : Estimated reward value of action till a particular time step 
         # format [qt(a), no of times selected]
-        # @Reward : list to store the reqard
+        # @Reward : list to store the reward
         gauss_sample = np.random.normal(size = BANDIT_SIZE)
         
-        self.q = {a: gauss_sample[a - 1] for a in range(0, BANDIT_SIZE)}
+        self.q = {a: gauss_sample[a - 1] for a in range(1, BANDIT_SIZE + 1)}
         self.epsilon = epsilon
-        self.qt = [(-float('inf'),0) for i in range(BANDIT_SIZE)]
+        self.qt = {a: (float('-inf'),0) for a in range(1, BANDIT_SIZE + 1)}
         self.reward = []
 
     def step(self):
-        if random.random()<self.epsilon: #Exploring other actions
-            indice_to_be_explored = random.randint(0, BANDIT_SIZE-1)
-            reward_obtained = self.getRewardValue(indice_to_be_explored)
+        if random.random() < self.epsilon: #Exploring other actions
+            curr_action = random.randint(1, BANDIT_SIZE)
+            reward_obtained = self.getRewardValue(curr_action)
             self.reward.append(reward_obtained)
-            self.update_average(indice_to_be_explored, reward_obtained)
-
+            self.updateAverage(curr_action, reward_obtained)
         else:
             #Greedy approach
-            max_element =  max(self.qt)
-            indice_having_maxqt = self.qt.index(max_element)
-            reward_obtained = self.getRewardValue(indice_having_maxqt)
+            max_reward_action = max(self.qt, key=self.qt.get)
+            reward_obtained = self.getRewardValue(max_reward_action)
             self.reward.append(reward_obtained)
-            self.update_average(indice_having_maxqt, reward_obtained)      
+            self.updateAverage(max_reward_action, reward_obtained)      
 
     def getRewardValue(self, a):
         return self.q[a] + np.random.normal()
 
-    def update_average(self, index, reward):
-        a,b = self.qt[index]
-        if b==0:
-            a = reward
-            b = b + 1
+    def updateAverage(self, index, reward):
+        avg, n = self.qt[index]
 
+        if n == 0:
+            avg = reward
+            n += 1
         else:
-            a = ((a*b)+reward)/(b+1)
-            b = b + 1
+            avg = ((avg*n)+reward)/(n+1)
+            n += 1
 
-        self.qt[index] = (a,b)
+        self.qt[index] = (avg, n)
 
-    def tellstate(self):
-        print(self.q, self.qt)
+    def getState(self):
+        return (self.q, self.qt)
 
 if __name__ == "__main__":
 
-    list_e_0 = [Bandit() for i in range(SAMPLE)]
-    for x in list_e_0:
+    tasks_e_0 = [Bandit() for i in range(SAMPLE)]
+    for bandit_task in tasks_e_0:
         for t in range(TIME_STEPS):
-            x.step()
+            bandit_task.step()
 
-    list_e_0_01 = [Bandit(epsilon=0.01) for i in range(SAMPLE)]
-    for x in list_e_0_01:
+    tasks_e_0_01 = [Bandit(epsilon=0.01) for i in range(SAMPLE)]
+    for bandit_task in tasks_e_0_01:
         for t in range(TIME_STEPS):
-            x.step()
+            bandit_task.step()
 
-    list_e_0_1 = [Bandit(epsilon=0.1) for i in range(SAMPLE)]
-    for x in list_e_0:
+    tasks_e_0_1 = [Bandit(epsilon=0.1) for i in range(SAMPLE)]
+    for bandit_task in tasks_e_0_1:
         for t in range(TIME_STEPS):
-            x.step()
+            bandit_task.step()
 
     #ITERATE THROUGH BANDITS of list_e_0_1 etc... 
     #AND ACCESS THEIR SELF.REWARDS AND TAKE MEAN TO GET VALUES FOR GRAPH
+
+    plot_reward_list = [mean([task.reward[i] for task in tasks_e_0]) for i in range(TIME_STEPS)]
+    plt.plot(plot_reward_list)
+    plt.show()
+
+    # Testing
+    # for i, bandit_task in enumerate(tasks_e_0):
+    #     q, qt = bandit_task.getState()
+    #     print(f"{i}:\n{q}\n{qt}")
+
+    # for i, bandit_task in enumerate(tasks_e_0_01):
+    #     q, qt = bandit_task.getState()
+    #     print(f"{i}:\n{q}\n{qt}")
+
+    # for i, bandit_task in enumerate(tasks_e_0_1):
+    #     q, qt = bandit_task.getState()
+    #     print(f"{i}:\n{q}\n{qt}")
