a13e496123c5d728b71d5884832f679fb669c00a
Aviral Gupta <aviralg1975@gmail.com>
2021-06-26 18:41:44 +0530

    Added func for general graphs and updated bandit class

--- Ch2/10_armed_testbed.py
+++ Ch2/10_armed_testbed.py
@@ -41,77 +41,35 @@ class Bandit:
         else:
             self.best_action_taken.append(True)
 
-        self.updateAverage(action, reward_obtained)
+        self.updateEstimate(action, reward_obtained)
 
     def getRewardValue(self, a):
         return self.q[a] + np.random.normal()
 
-    def updateAverage(self, index, reward):
-        avg, n = self.qt[index]
+    def updateEstimate(self, index, reward):
+        qk, n = self.qt[index]
+        step_size = 0.1
 
         if n == 0:
-            avg = reward
+            qk = reward
             n += 1
         else:
-            avg = ((avg*n)+reward)/(n+1)
+            qk += (reward - qk)*step_size
             n += 1
 
-        self.qt[index] = (avg, n)
+        self.qt[index] = (qk, n)
 
     def getState(self):
         return (self.q, self.qt)
 
 
-def avg_reward_graph(tasks_1, tasks_2, tasks_3):
-    plot_reward_list_1 = [mean([task.reward[i] for task in tasks_1]) for i in range(TIME_STEPS)]
-    plot_reward_list_2 = [mean([task.reward[i] for task in tasks_2]) for i in range(TIME_STEPS)]
-    plot_reward_list_3 = [mean([task.reward[i] for task in tasks_3]) for i in range(TIME_STEPS)]
+def avg_reward_general(graphs):
+    for lst in graphs:
+        plot_reward_list = [mean([task.reward[i] for task in lst]) for i in range(TIME_STEPS)]
+        plt.plot(plot_reward_list, label=f'epsilon = {lst[0].epsilon}, initial = {lst[0].initial}')
 
-    plt.subplot(1, 2, 1)
-    plt.plot(plot_reward_list_1, label=f'epsilon = {tasks_1[0].epsilon}')
-    plt.plot(plot_reward_list_2, label=f'epsilon = {tasks_2[0].epsilon}')
-    plt.plot(plot_reward_list_3, label=f'epsilon = {tasks_3[0].epsilon}')
-
-    plt.title('Average Reward through learning time steps')
     plt.xlabel('Steps')
     plt.ylabel('Average Reward')
-    plt.legend()
-
-def optimal_action_graph(tasks_1, tasks_2, tasks_3):
-    plot_best_action_1 = []
-    plot_best_action_2 = []
-    plot_best_action_3 = []
-
-    for i in range(TIME_STEPS):
-        n1 = 0
-        for task in tasks_1:
-            if task.best_action_taken[i]:
-                n1 += 1
-
-        plot_best_action_1.append((n1/SAMPLE)*100)
-
-        n2 = 0
-        for task in tasks_2:
-            if task.best_action_taken[i]:
-                n2 += 1
-
-        plot_best_action_2.append((n2/SAMPLE)*100)
-
-        n3 = 0
-        for task in tasks_3:
-            if task.best_action_taken[i]:
-                n3 += 1
-
-        plot_best_action_3.append((n3/SAMPLE)*100)
-    
-    plt.subplot(1, 2, 2)
-    plt.plot(plot_best_action_1, label=f'epsilon = {tasks_1[0].epsilon}')
-    plt.plot(plot_best_action_2, label=f'epsilon = {tasks_2[0].epsilon}')
-    plt.plot(plot_best_action_3, label=f'epsilon = {tasks_3[0].epsilon}')
-
-    plt.title('Percentage of actions where best actions is chosen through learning time steps')
-    plt.xlabel('Steps')
-    plt.ylabel('% Optimal action')
     plt.legend()
 
 def optimal_action_general(graphs):
@@ -125,11 +83,24 @@ def optimal_action_general(graphs):
             plot_best_action.append((n/SAMPLE)*100)
         
         plt.plot(plot_best_action, label=f'epsilon = {lst[0].epsilon}, initial = {lst[0].initial}')
-
-    plt.title('Percentage of actions where best actions is chosen through learning time steps')
+    
     plt.xlabel('Steps')
     plt.ylabel('% Optimal action')
     plt.legend()
+
+# Fig 2.1 graph
+def avg_reward_graph(tasks_1, tasks_2, tasks_3):
+    plt.subplot(1, 2, 1)
+    avg_reward_general((tasks_1, tasks_2, tasks_3))
+
+    plt.title('Average Reward through learning time steps')
+
+#Fig 2.1 graph
+def optimal_action_graph(tasks_1, tasks_2, tasks_3):
+    plt.subplot(1, 2, 2)
+    optimal_action_general((tasks_1, tasks_2, tasks_3))
+
+    plt.title('Percentage of actions where best actions is chosen through learning time steps')
 
 def sim(epsilon = 0, initial = 0):
     tasks = [Bandit(epsilon=epsilon, initial=initial) for i in range(SAMPLE)]
@@ -140,24 +111,38 @@ def sim(epsilon = 0, initial = 0):
     return tasks
 
 if __name__ == "__main__":
+    # SIMULATE LEARNING
 
-    tasks_e_0_0 = sim()
-    tasks_e_0__inf = sim(initial=float("inf"))
-    tasks_e_0_inf = sim(initial=float("-inf"))
-    tasks_e_0_01_0 = sim(epsilon = 0.01, initial=0)
-    tasks_e_0_01__inf = sim(epsilon = 0.01, initial=float("inf"))
-    tasks_e_0_01_inf = sim(epsilon = 0.01, initial=float("-inf"))
-    tasks_e_0_1_0 = sim(epsilon = 0.1, initial=0)
-    tasks_e_0_1__inf = sim(epsilon = 0.1, initial=float("inf"))
-    tasks_e_0_1_inf = sim(epsilon = 0.1, initial=float("-inf"))
+    tasks_e_0 = sim()
+    tasks_e_0_01 = sim(epsilon = 0.01)
+    tasks_e_0_1 = sim(epsilon = 0.1)
 
-    # Show graphs
+    # tasks_e_0_0 = sim()
+    # tasks_e_0__inf = sim(initial=float("inf"))
+    # tasks_e_0_inf = sim(initial=float("-inf"))
+    # tasks_e_0_01_0 = sim(epsilon = 0.01, initial=0)
+    # tasks_e_0_01__inf = sim(epsilon = 0.01, initial=float("inf"))
+    # tasks_e_0_01_inf = sim(epsilon = 0.01, initial=float("-inf"))
+    # tasks_e_0_1_0 = sim(epsilon = 0.1, initial=0)
+    # tasks_e_0_1__inf = sim(epsilon = 0.1, initial=float("inf"))
+    # tasks_e_0_1_inf = sim(epsilon = 0.1, initial=float("-inf"))
+
+    # task_e_0_5 = sim(initial=5)
+    # tasks_e_0_1_0 = sim(epsilon=0.1)
+    # tasks_e_0_10 = sim(initial=10)
+    # tasks_e_0_20 = sim(initial=20)
+    # task_e_0__inf = sim(initial=float('inf'))
+
+    # SHOW GRAPHS
     # plt.figure(figsize=(17, 7))
-    # avg_reward_graph(tasks_e_0, tasks_e_0_01, tasks_e_0_1)
-    optimal_action_general((tasks_e_0_0, tasks_e_0__inf, tasks_e_0_inf, tasks_e_0_01_0, tasks_e_0_01__inf, tasks_e_0_01_inf, tasks_e_0_1_0, tasks_e_0_1__inf, tasks_e_0_1_inf))
+
+    avg_reward_graph(tasks_e_0, tasks_e_0_1, tasks_e_0_01)
+    optimal_action_graph(tasks_e_0, tasks_e_0_01, tasks_e_0_1)
+    # optimal_action_general((task_e_0_5, tasks_e_0_1_0, task_e_0__inf, tasks_e_0_10, tasks_e_0_20))
+
     plt.show()
 
-    # Testing
+    # TESTING
     # for i, bandit_task in enumerate(tasks_e_0):
     #     q, qt = bandit_task.getState()
     #     print(f"{i}:\n{q}\n{qt}")
