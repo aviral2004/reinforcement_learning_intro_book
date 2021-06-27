3d5bb5bf1a557176349e0db935092b40afb9ad86
Aviral Gupta <aviralg1975@gmail.com>
2021-06-28 02:04:03 +0530

    Added basic funcs for gradient. Have to complete preferences function.

--- Ch2/10_armed_testbed.py
+++ Ch2/10_armed_testbed.py
@@ -3,13 +3,14 @@ import random
 import matplotlib as mpt
 from matplotlib import pyplot as plt
 from statistics import mean
+from math import e
 
 BANDIT_SIZE = 10
-TIME_STEPS  = 1000
-SAMPLE      = 20000
+TIME_STEPS  = 5
+SAMPLE      = 2
 
 class Bandit:
-    def __init__(self, epsilon = 0, initial = 0):
+    def __init__(self, epsilon = 0, initial = 0, step_size = 0.1, baseline = False):
         # @q      : Real reward value of actions
         # @epsilon: probability for exploration
         # @qt     : Estimated reward value of action till a particular time step 
@@ -18,14 +19,24 @@ class Bandit:
         gauss_sample = np.random.normal(size = BANDIT_SIZE)
         
         self.q = {a: gauss_sample[a - 1] for a in range(1, BANDIT_SIZE + 1)}
+        self.qt = {a: (initial, 0) for a in range(1, BANDIT_SIZE + 1)}
+
         self.epsilon = epsilon
         self.initial = initial
-        self.qt = {a: (initial, 0) for a in range(1, BANDIT_SIZE + 1)}
+        self.step_size = step_size
+        self.gradient_baseline = baseline
+
+        self.avg_reward = 0
+        self.time_step = 0
+
         self.reward = []
+
         self.best_action_taken = []
         self.best_action = max(self.q, key=self.q.get)
 
-    def step(self):
+        self.preferences = {a: 0 for a in range(1, BANDIT_SIZE + 1)}
+
+    def e_greedy(self):
         if random.random() < self.epsilon:
             #Exploring other actions
             action = random.randint(1, BANDIT_SIZE)
@@ -33,6 +44,13 @@ class Bandit:
             #Greedy approach
             action = max(self.qt, key=self.qt.get)
 
+        return action
+
+    def gradientPref(self):
+        return
+
+    def step(self):
+        action = self.e_greedy()
         reward_obtained = self.getRewardValue(action)
 
         self.reward.append(reward_obtained)
@@ -43,10 +61,32 @@ class Bandit:
 
         self.updateEstimate(action, reward_obtained)
 
+        self.gradientUpdate(action, reward_obtained)
+
+        self.time_step += 1
+        self.updateAvgReward(reward_obtained)
+
     def getRewardValue(self, a):
         return self.q[a] + np.random.normal()
 
-    def updateEstimate(self, index, reward):
+    def updateAvgReward(self, reward):
+        self.avg_reward += (1/self.time_step)*(reward - self.avg_reward)
+
+    def _calc_prob(self, action_index):
+        prob_t = np.exp(self.preferences[action_index])/sum([np.exp(val) for val in self.preferences.values()])
+        return prob_t
+
+    def gradientUpdate(self, index, reward):
+        for action in self.preferences:
+            var = 1 if action == index else 0
+            if self.gradient_baseline:
+                baseline = self.avg_reward
+            else:
+                baseline = 0
+
+            self.preferences[action] += self.step_size*(var - self._calc_prob(action))*(reward - baseline)
+    
+    def sampleAverages(self, index, reward):
         qk, n = self.qt[index]
 
         if n == 0:
@@ -54,13 +94,17 @@ class Bandit:
             n += 1
         else:
             step_size = 1/n
-            qk += (reward - qk)*step_size
+            qk += ((reward - qk) * step_size)
             n += 1
 
         self.qt[index] = (qk, n)
 
+    def updateEstimate(self, index, reward):
+        self.sampleAverages(index, reward)
+
     def getState(self):
         return (self.q, self.qt)
+        # return {i:self._calc_prob(i) for i in self.preferences}
 
 
 def avg_reward_general(graphs):
@@ -102,11 +146,12 @@ def optimal_action_graph(tasks_1, tasks_2, tasks_3):
 
     plt.title('Percentage of actions where best actions is chosen through learning time steps')
 
-def sim(epsilon = 0, initial = 0):
-    tasks = [Bandit(epsilon=epsilon, initial=initial) for i in range(SAMPLE)]
+def sim(eps = 0, init = 0):
+    tasks = [Bandit(epsilon=eps, initial=init) for i in range(SAMPLE)]
     for task in tasks:
         for t in range(TIME_STEPS):
             task.step()
+            print(task.getState())
 
     return tasks
 
@@ -114,30 +159,31 @@ if __name__ == "__main__":
     # SIMULATE LEARNING
 
     tasks_e_0 = sim()
-    tasks_e_0_01 = sim(epsilon = 0.01)
-    tasks_e_0_1 = sim(epsilon = 0.1)
+    tasks_e_0_01 = sim(eps = 0.01)
+    tasks_e_0_1 = sim(eps = 0.1)
 
     # tasks_e_0_0 = sim()
-    # tasks_e_0__inf = sim(initial=float("inf"))
-    # tasks_e_0_inf = sim(initial=float("-inf"))
-    # tasks_e_0_01_0 = sim(epsilon = 0.01, initial=0)
-    # tasks_e_0_01__inf = sim(epsilon = 0.01, initial=float("inf"))
-    # tasks_e_0_01_inf = sim(epsilon = 0.01, initial=float("-inf"))
-    # tasks_e_0_1_0 = sim(epsilon = 0.1, initial=0)
-    # tasks_e_0_1__inf = sim(epsilon = 0.1, initial=float("inf"))
-    # tasks_e_0_1_inf = sim(epsilon = 0.1, initial=float("-inf"))
+    # tasks_e_0__inf = sim(init=float("inf"))
+    # tasks_e_0_inf = sim(init=float("-inf"))
+    # tasks_e_0_01_0 = sim(eps = 0.01, init=0)
+    # tasks_e_0_01__inf = sim(eps = 0.01, init=float("inf"))
+    # tasks_e_0_01_inf = sim(eps = 0.01, init=float("-inf"))
+    # tasks_e_0_1_0 = sim(eps = 0.1, init=0)
+    # tasks_e_0_1__inf = sim(eps = 0.1, init=float("inf"))
+    # tasks_e_0_1_inf = sim(eps = 0.1, init=float("-inf"))
 
-    # task_e_0_5 = sim(initial=5)
-    # tasks_e_0_1_0 = sim(epsilon=0.1)
-    # tasks_e_0_10 = sim(initial=10)
-    # tasks_e_0_20 = sim(initial=20)
-    # task_e_0__inf = sim(initial=float('inf'))
+    # task_e_0_5 = sim(init=5)
+    # tasks_e_0_1_0 = sim(eps=0.1)
+    # tasks_e_0_10 = sim(init=10)
+    # tasks_e_0_20 = sim(init=20)
+    # task_e_0__inf = sim(init=float('inf'))
 
     # SHOW GRAPHS
     # plt.figure(figsize=(17, 7))
 
     avg_reward_graph(tasks_e_0, tasks_e_0_1, tasks_e_0_01)
     optimal_action_graph(tasks_e_0, tasks_e_0_01, tasks_e_0_1)
+    # optimal_action_general((tasks_e_0_1_0, task_e_0_5))
     # optimal_action_general((task_e_0_5, tasks_e_0_1_0, task_e_0__inf, tasks_e_0_10, tasks_e_0_20))
 
     plt.show()
